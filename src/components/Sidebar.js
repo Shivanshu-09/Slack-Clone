e3987a300f34e76d4e79eb3ca509501dbcabee7c
Shivanshu-09 <shivanshusarin@gmail.com>
2020-10-22 10:21:14 +0530

    Added styles

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,7 +1,28 @@
 import React, { Component } from 'react';
 
-export default class Sidebar extends Component {
+class Sidebar extends Component {
   render() {
-    return <div>Side bar</div>;
+    return (
+      <div id="sidebar">
+        <div className="user-profile">
+          <div className="avatar">
+            <img src="https://www.flaticon.com/svg/static/icons/svg/2919/2919600.svg" />
+          </div>
+          <div>Ronald</div>
+        </div>
+        <hr className="sidebar-spacer" />
+
+        <div className="channels">
+          <div className="header">Channels</div>
+
+          <ul className="channels-list">
+            <li># assignment</li>
+            <li># test</li>
+          </ul>
+        </div>
+      </div>
+    );
   }
 }
+
+export default Sidebar;
